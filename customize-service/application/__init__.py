82e154735ceebe278ea4f1d15e264a7fd887fd16
Ubuntu <ubuntu@ip-172-31-95-24.ec2.internal>
2024-02-08 14:36:39 +0000

    remove bad code

--- customize-service/application/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-from flask import render_template
-
-app = Flask(__name__)
-from application import routes
--- customize-service/application/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template, redirect, url_for, flash
-from application import app
-import json
-
-
-@app.route('/')
-@app.route('/home')
-def home_page():
-    return render_template('home_page.html')
