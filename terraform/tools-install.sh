b3b746d961e07ee3fb52acc507ddb55e4c7f8881
Kelvinskell <kelvinskelll@gmail.com>
2024-02-10 12:54:48 +0100

    Update user data

--- terraform/tools-install.sh
+++ terraform/tools-install.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Install Docker
-
-sudo apt-get update -y 
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu jammy stable" -y
-apt-cache policy docker-ce
-sudo apt install docker-ce -y
-sudo usermod -aG docker ubuntu
-
 # Install Jenkins
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
 sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
@@ -19,8 +9,18 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt update
-sudo apt-get install openjdk-11-jdk -y
+sudo apt-get install openjdk-17-jdk -y
 sudo apt install jenkins -y
+
+# Install Docker
+
+sudo apt-get update -y 
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu jammy stable" -y
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo usermod -aG docker jenkins
 
 # Install Trivy
 sudo apt-get install wget apt-transport-https gnupg lsb-release -y
